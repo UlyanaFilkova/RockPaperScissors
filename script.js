945eafa32c7786fa7ccea25d9aaa4b95b13e8701
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-04-14 12:14:03 +0300

    do logic

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-function playRound(playerSelection, computerSelection) {
+function paintChosenButton(button, color) {
+    button.style.backgroundColor = color;
+}
+
+//   игрок победил 1
+//   компьютер победил 0
+//   ничья (одинаково) 2
+function whoWins(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 2;
     }
@@ -36,18 +43,80 @@ function playRound(playerSelection, computerSelection) {
             break;
         }
         default:
-            return "Неверный";
+            return 2;
     }
 }
-//   камень 1
-//   ножницы 2
-//   бумага 3
 
-//   игрок победил 1
-//   компьютер победил 0
-// ничья (одинаково) 2
-const playerSelection = 1;
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+function getName(number) {
+    switch (number) {
+        case 1: {
+            return 'Камень';
+        }
+        case 2: {
+            return 'Ножницы';
+        }
+        case 3: {
+            return 'Бумага';
+        }
+    }
+}
 
-console.log(playRound(playerSelection, computerSelection));
+function createResultText(winner, loser) {
+    winner = Number(winner);
+    loser = Number(loser);
+
+    if (winner === loser) {
+        return "Ничья!";
+    }
+
+    let text = getName(winner);
+
+    if (winner === 2) {
+        text = text.concat(' бьют ');
+    } else {
+        text = text.concat(' бьёт ');
+    }
+
+    text = text.concat(getName(loser));
+    if (loser === 3) {
+        text = text.substring(0, text.length - 1) + 'у';
+    }
+
+    return text;
+}
+
+document.querySelectorAll('#options>button').
+    forEach((button) => {
+        button.addEventListener('click', function (event) {
+            const userButton = event.target;
+            const userChoice = userButton.value;
+            paintChosenButton(event.target, 'grey');
+
+            const pcChoice = getComputerChoice();
+            const pcButton = document.querySelector(`#pcOptions button[value="${pcChoice}"]`);
+            setTimeout(function () {
+                paintChosenButton(pcButton, 'grey')
+            }, 500);
+            const userWins = whoWins(userChoice, pcChoice);
+            let resultText = '';
+            switch (userWins) {
+                case 1: {
+                    paintChosenButton(userButton, 'green');
+                    paintChosenButton(pcButton, 'red');
+                    resultText = createResultText(userChoice, pcChoice);
+                }
+                case 0: {
+                    paintChosenButton(userButton, 'red');
+                    paintChosenButton(pcButton, 'green');
+                    resultText = createResultText(pcChoice, userChoice);
+                }
+                case 2: {
+                    resultText = createResultText(userChoice, pcChoice);
+                }
+            }
+
+            const resultDiv = document.getElementById('result');
+            resultDiv.textContent = resultText;
+        });
+    });
+
